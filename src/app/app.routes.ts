8b91b0f098bd6df4c395849e541d5d256788c196
yaromochka <137483082+yaromochka@users.noreply.github.com>
2025-05-26 10:48:00 +0300

    feat: make start page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import {UsersPageComponent} from '../pages/users-page/users-page.component';
 import {ProfilePageComponent} from '../pages/profile-page/profile-page.component';
 import {AppComponent} from './app.component';
 import {NewPersonPageComponent} from '../pages/new-person-page/new-person-page.component';
+import {StartPageComponent} from '../pages/start-page/start-page.component';
 
 export const routes: Routes = [
+  {path: '', component: StartPageComponent},
   {path: 'users', component: UsersPageComponent},
   {path: 'users/add', component: NewPersonPageComponent},
   {path: 'users/:id', component: ProfilePageComponent},
---
+++ src/pages/start-page/start-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-start-page',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './start-page.component.html',
+  styleUrl: './start-page.component.scss'
+})
+export class StartPageComponent {
+
+}
