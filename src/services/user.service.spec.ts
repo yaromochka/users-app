b3c9d94e490b2acf638c943a8127b9c0def8d1de
yaromochka <137483082+yaromochka@users.noreply.github.com>
2025-05-27 09:04:53 +0300

    feat: add adaptive

--- src/services/user.service.spec.ts
+++ src/services/user.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import {User} from '../models/user/user';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
+import {provideHttpClient} from '@angular/common/http';
 
-describe('UserServiceService', () => {
+describe('UserService', () => {
   let service: UserService;
+  let user: User[];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        provideHttpClient(),
+        provideHttpClientTesting()],
+      providers: [UserService]
+    });
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should ', () => {
+    service.getUsers().subscribe(users => {
+      user = users;
+    })
+    expect(user.length).toBeGreaterThan(0);
+  })
 });
